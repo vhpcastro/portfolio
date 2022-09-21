52eef5b53e3f072841bb85fc2d612fe1f7a15f3b
Victor Hugo <vhpcastro0811@gmail.com>
2022-09-21 19:40:45 -0300

    Estruturando a base do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Moldura from './components/moldura/moldura';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="box">
+          <div className="container01">
+            <Moldura />
+          </div>
+
+          <div className="container02">
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
