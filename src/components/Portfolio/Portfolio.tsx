6ed68eb7334c86096b2ff67bef9cc759effce520
Victor Hugo <vhpcastro0811@gmail.com>
2022-09-28 20:52:48 -0300

    Criando e estilizando componente Portfolio

---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './Portfolio.css';
+
+function Portfolio() {
+
+    return (
+        <>
+
+            <div className="portfolio-container">
+
+                <div className="portfolio-content">
+
+                    <h1>PORTFOLIO</h1>
+
+                    <div className="portfolio-box">
+
+                        <div className="project">
+                            <img src="https://cdn.discordapp.com/attachments/710276943592816720/1024799968235622451/Telk-logos.jpeg" alt="" />
+                        </div>
+
+                        <div className="project">
+
+                        </div>
+
+                        <div className="project">
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Portfolio;
