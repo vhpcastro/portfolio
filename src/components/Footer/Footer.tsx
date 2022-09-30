24c32edb5e41b2d44bc63315f6ce212fa312afea
Victor Hugo <vhpcastro0811@gmail.com>
2022-09-30 20:00:43 -0300

    Criando e estilizando componente Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+
+    return (
+
+        <>
+            <div className="footer-container">
+
+                <div className="footer-content">
+
+                    <div className="footer-box01">
+                        <p>Copyright © 2022 Victor Hugo Personal Profile</p>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div className="footer-box02">
+                        <div className="icons8-github"></div>
+                        <div className="icons8-linkedin-2"></div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+
+    )
+}
+
+export default Footer;
